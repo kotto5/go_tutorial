96a8ca12473e08d71b4bec30d9b8196e9a10cc50
kotto5 <kotto555555@gmail.com>
2024-03-29 18:42:39 +0900

    error handling go function return 2 values.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello return a greeting for the named person.
-func Hello(name string) string {
-	// return a greeting that embeds the name in a message.
+func Hello(name string) (string, error) {
+	// if no name was given, return an error message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// if a name was received, return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
